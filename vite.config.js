7dc09deab7627b31ed774ad0f0bfd5086f21472c
rezpecktor <rezpecktor284@gmai.com>
2025-07-19 17:50:46 +0700

    gemini update 1745 new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+// --- TAMBAHKAN BARIS DI BAWAH INI ---
+import path from "path";
+// ------------------------------------
 
 export default defineConfig({
     plugins: [
@@ -19,11 +22,16 @@ export default defineConfig({
             },
         }),
     ],
-    // --- TAMBAHKAN BLOK DI BAWAH INI ---
     resolve: {
         alias: {
-            "ziggy-js": "/vendor/tightenco/ziggy/dist/vue.m",
+            // --- GANTI BARIS DI BAWAH INI ---
+            // 'ziggy-js': '/vendor/tightenco/ziggy/dist/vue.m',
+            // --- MENJADI SEPERTI INI ---
+            "ziggy-js": path.resolve(
+                __dirname,
+                "vendor/tightenco/ziggy/dist/vue.m"
+            ),
+            // ----------------------------
         },
     },
-    // ------------------------------------
 });
